eec05ca4bf64a261f6a9ba78fd45e4b300bbc4af
ken <kenlin@gotutor4u.com>
2019-08-09 20:41:07 +0800

    vue-gtm

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import VueGtm from 'vue-gtm';
 import html2canvas from 'html2canvas';
 
 Vue.config.productionTip = false
@@ -9,3 +10,8 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+Vue.use(VueGtm, {
+  id: 'GTM-THPTKXQ', // Your GTM ID
+  enabled: true, // defaults to true. Plugin can be disabled by setting this to false for Ex: enabled: !!GDPR_Cookie (optional)
+  debug: true // Whether or not display console logs debugs (optional)
+});
